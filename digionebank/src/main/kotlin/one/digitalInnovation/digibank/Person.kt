c589b2cf37156945bf31f6e7de35546cc7c7e88b
Reinaldo Couto <reijunior1@hotmail.com>
2021-02-16 14:31:58 -0300

    initial bankdigione

--- digionebank/src/main/kotlin/one/digitalInnovation/digibank/Person.kt
+++ digionebank/src/main/kotlin/one/digitalInnovation/digibank/Person.kt
@@ -4,10 +4,3 @@ class Person {
     var name: String = "Reinaldo"
     var cpf: String = "111.222.555-44"
 }
-
-fun main(){
-    val reinaldo = Person()
-
-    println(reinaldo.name)
-    print(reinaldo.cpf)
-}
